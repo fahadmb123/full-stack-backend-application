afa005b22298f5b1640017cf53de4bcd721bf222
Fahad MB <fahadmbasheer@gmail.com>
2026-08-12 23:34:01 +0530

    fix:change server with dns

--- src/app.ts
+++ src/app.ts
@@ -3,6 +3,11 @@ import express from "express"
 const app = express();
 
 
+const dns = require("dns")
+console.log(dns.getServers());
+dns.setServers(["8.8.8.8", "8.8.4.4"]);
+
+
 app.use(express.json())
 
 
