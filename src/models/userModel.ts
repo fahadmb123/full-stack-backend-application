02765ac95b5935497b050d20aac6f4b6ed03ecf7
Fahad MB <fahadmbasheer@gmail.com>
2026-08-13 21:19:46 +0530

    feat : Add user MongoDB model

---
+++ src/models/userModel.ts
@@ -0,0 +1,33 @@
+import mongoose, { Schema } from "mongoose";
+
+const userSchema = new Schema({
+    userId: {
+        type: Number,
+        required: true,
+        unique: true
+    },
+
+    username: {
+        type: String,
+        required: true
+    },
+
+    email: {
+        type: String,
+        required: true,
+        unique: true
+    },
+
+    password: {
+        type: String,
+        required: true
+    },
+
+    role: {
+        type: String,
+        enum: ["user", "admin"],
+        required: true
+    }
+});
+
+export const UserModel = mongoose.model("User", userSchema);
