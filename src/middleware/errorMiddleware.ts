fd436d2a2025803607a97e24144da9845738e1d0
Fahad MB <fahadmbasheer@gmail.com>
2026-08-14 20:46:03 +0530

    chor : Update errorMiddleware

--- src/middleware/errorMiddleware.ts
+++ src/middleware/errorMiddleware.ts
@@ -1,8 +1,17 @@
 import { Request,Response,NextFunction } from "express";
+import { AppError } from "../errors/AppError";
 
 
 
 const errorMiddleware = (err:Error, req:Request, res:Response, next:NextFunction) => {
+
+    if (err instanceof AppError){
+        res.status(err.statusCode).json({
+            success:false,
+            message:err.message
+        })
+        return 
+    }
     res.status(500).json({
         success: false,
         message: err.message
