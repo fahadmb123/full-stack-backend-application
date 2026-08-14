f2992afa7b9186c4927d09bed4f7238e69003e7b
Fahad MB <fahadmbasheer@gmail.com>
2026-08-14 18:36:21 +0530

    feat : add auth middleware

--- src/app.ts
+++ src/app.ts
@@ -1,5 +1,7 @@
 import express from "express"
 import userRoute from "./routes/userRoutes"
+import cookieParser from "cookie-parser";
+
 const app = express();
 
 
@@ -9,6 +11,7 @@ dns.setServers(["8.8.8.8", "8.8.4.4"]);
 
 
 app.use(express.json())
+app.use(cookieParser());
 
 app.use("/api/auth",userRoute)
 
---
+++ src/middleware/authMiddleware.ts
@@ -0,0 +1,35 @@
+import { Request, Response, NextFunction } from "express";
+import { verifyToken } from "../utils/jwt";
+
+export const authMiddleware = (
+    req: Request,
+    res: Response,
+    next: NextFunction
+): void => {
+
+    try {
+
+        const token = req.cookies.token;
+
+        if (!token) {
+            res.status(401).json({
+                success: false,
+                message: "Authentication required"
+            });
+            return;
+        }
+
+        const decoded = verifyToken(token);
+
+        req.user = decoded;
+
+        next();
+
+    } catch (error) {
+
+        res.status(401).json({
+            success: false,
+            message: "Invalid or expired token"
+        });
+    }
+};
