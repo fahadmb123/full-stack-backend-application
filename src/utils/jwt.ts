4c90358cadea842130b5c6213f1bf18a5b371687
Fahad MB <fahadmbasheer@gmail.com>
2026-08-14 13:53:39 +0530

    feat : add jwt

---
+++ src/utils/jwt.ts
@@ -0,0 +1,10 @@
+import jwt from "jsonwebtoken";
+
+export const generateToken = (userId: number, role: string): string => {
+
+    return jwt.sign(
+        {userId,role}
+        ,process.env.JWT_SECRET!,
+        { expiresIn: "7d"}
+    );
+};
