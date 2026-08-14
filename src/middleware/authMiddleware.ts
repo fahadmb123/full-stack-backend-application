e6da54d56bf1211de6ef0346722afe82f3c0ed2a
Fahad MB <fahadmbasheer@gmail.com>
2026-08-14 20:15:24 +0530

    Updated

--- src/middleware/authMiddleware.ts
+++ src/middleware/authMiddleware.ts
@@ -8,7 +8,7 @@ export const authMiddleware = (req: Request,res: Response,next: NextFunction): v
             res.status(401).json({
                 success: false,
                 message: "Authentication required"
-            });
+            })
             return
         }
         verifyToken(token)
@@ -19,5 +19,23 @@ export const authMiddleware = (req: Request,res: Response,next: NextFunction): v
             message: "Invalid or expired token"
         })
         return
+    }
+}
+
+export const guestMiddleware = (req: Request,res: Response,next: NextFunction): void => {
+    try {
+        const token = req.cookies.token
+        if (!token) {
+            next()
+            return
+        }
+        verifyToken(token)
+        res.status(409).json({
+            success: false,
+            message: "You are already logged in"
+        })
+        return
+    } catch (error) {
+        next(error);
     }
 };
