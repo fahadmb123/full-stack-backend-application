2c238e687505b5c890bb2b397351ccd25f9c91f7
Fahad MB <fahadmbasheer@gmail.com>
2026-08-14 22:52:31 +0530

    Update

--- src/routes/adminRoutes.ts
+++ src/routes/adminRoutes.ts
@@ -1,9 +1,10 @@
 import express from "express"
 import { adminController } from "../container"
+import { authMiddleware } from "../middleware/authMiddleware"
 const router = express.Router()
 
 
-router.get("/users",adminController.getAllUsers)
+router.get("/users",authMiddleware,adminController.getAllUsers)
 
 
 export default router
