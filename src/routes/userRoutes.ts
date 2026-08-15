bbcbf4413c7fc695fdb8bb71ce969cdbb1de5c38
Fahad MB <fahadmbasheer@gmail.com>
2026-08-15 12:38:45 +0530

    feat : add validate middleware

--- src/routes/userRoutes.ts
+++ src/routes/userRoutes.ts
@@ -2,13 +2,15 @@ import express from "express"
 import { userController } from "../container"
 import { authMiddleware,guestMiddleware } from "../middleware/authMiddleware"
 import { roleMiddleware } from "../middleware/roleMiddleware"
+import { loginSchema,registerSchema } from "../schemas/auth.schema"
+import validate from "../middleware/validate"
 const router = express.Router()
 
 
 
 
-router.post("/register",guestMiddleware,userController.register)
-router.post("/login",guestMiddleware,userController.login)
+router.post("/register",validate(registerSchema),guestMiddleware,userController.register)
+router.post("/login",validate(loginSchema),guestMiddleware,userController.login)
 
 
 router.get("/products",authMiddleware,roleMiddleware(["user"]),userController.products)
---
+++ src/schemas/auth.schema.ts
@@ -0,0 +1,42 @@
+import {z} from "zod"
+
+
+export const registerSchema = z
+.object({
+    name: z
+        .string()
+        .min(2, "Name must be at least 2 characters"),
+
+    email: z
+        .string()
+        .email("Invalid email address"),
+
+    password: z
+        .string()
+        .min(6, "Password must be at least 6 characters"),
+
+    confirmPassword: z
+        .string()
+        .min(6, "Confirm password must be at least 6 characters"),
+})
+.refine(
+    (data) => data.password === data.confirmPassword,
+    {
+        message: "Passwords do not match",
+        path: ["confirmPassword"],
+    }
+)
+
+
+export const loginSchema = z.object({
+  email: z
+    .string()
+    .trim()
+    .min(1, "Email is required")
+    .email("Invalid email address"),
+
+  password: z
+    .string()
+    .min(1, "Password is required")
+    .min(6, "Password must be at least 6 characters"),
+});
