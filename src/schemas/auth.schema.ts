ef57917b7480d68e1531f01fb622bed6bc2841a2
Fahad MB <fahadmbasheer@gmail.com>
2026-08-15 12:39:33 +0530

    feat : add admin new user schema

--- src/schemas/auth.schema.ts
+++ src/schemas/auth.schema.ts
@@ -39,4 +39,11 @@ export const loginSchema = z.object({
     .string()
     .min(1, "Password is required")
     .min(6, "Password must be at least 6 characters"),
+})
+
+
+export const adminCreateUserSchema = z.object({
+    name: z.string().min(2, "Name must be at least 2 characters"),
+    email: z.string().email("Invalid email address"),
+    password: z.string().min(6, "Password must be at least 6 characters")
 });
